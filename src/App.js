8fcda251278744d1db270700646630d96002bed7
darkroom51 <darkroom@wp.pl>
2018-01-12 12:38:09 +0100

    day2; state counter with handlers

--- src/App.js
+++ src/App.js
@@ -6,16 +6,35 @@ class App extends Component {
         counter : 0
     }
 
-    componentDidMount(){
-        this.setState({
-            counter : 5
+    componentDidMount(){ // function componentDidMount() to samo co componentDidMount = () => {}, ale to reactowe funkcje i maja zbindowany this, wiec moga bys funkcjami zwyklymi a nie strzalkowymi
+        this.setState({ //tylko w ten sposob zmianiamy properties state'u, nigdy beposrednio
+            counter : this.state.counter + 1
+        }, () => console.log('state has been updated'))
+    }
+
+    onMinusClickHandler  = () => {
+        this.setState({ //tylko w ten sposob zmianiamy properties state'u, nigdy beposrednio
+            counter : this.state.counter - 1
+        }, () => console.log('state has been updated'))
+    }
+
+    onPlusClickHandler  = () => {
+        this.setState({ //tylko w ten sposob zmianiamy properties state'u, nigdy beposrednio
+            counter : this.state.counter + 1
         }, () => console.log('state has been updated'))
     }
 
     render() {
+        console.log('Render!', this.state)
         return (
             <div>
                 <h1>{this.state.counter}</h1>
+                <button onClick={this.onMinusClickHandler}>
+                    -
+                </button>
+                <button onClick={this.onPlusClickHandler}>
+                    +
+                </button>
             </div>
         )
     }
